9b8015f60dbc011c21afdccdba95bad1954be8c5
Soha <sohaelshafey@yahoo.com>
2024-12-24 10:42:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,9 +11,11 @@ st.sidebar.header("Parameters")
 Input_Answer = st.sidebar.text_area("Text to Enter")
 Golden_Answer = st.sidebar.text_area("Answers to Compare seprated by ;")
 if  st.sidebar.button("Analyze"):
-    if (len(Golden_Answer.split(';')) >= 1):
-        lst = Golden_Answer.split(';')
-        k = KEM(Input_Answer, lst)
+    if ';' not in Golden_Answer:
+        k = KEM(Input_Answer, Golden_Answer.strip())
+        k.Extract_and_match()
+    elif (len(Golden_Answer.split(';')) >= 1)
+        k = KEM(Input_Answer, Golden_Answer.split(';'))
         k.Extract_and_match()
     else:
         k = KEM(Input_Answer)
